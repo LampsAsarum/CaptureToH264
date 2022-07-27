494e8c0d530a7837d991161926a20f22402a30d6
xingzixin <xingzixin@cvte.com>
2022-07-27 08:38:34 +0800

    [feature]补充一些注释

--- CaptureToH264/CaptureToH264/Convert.cpp
+++ CaptureToH264/CaptureToH264/Convert.cpp
@@ -2,7 +2,7 @@
 #include <Windows.h>
 #include "libyuv/convert_from_argb.h"
 
-namespace 
+namespace
 {
     unsigned char ClipValue(unsigned char x, unsigned char min_val, unsigned char  max_val) {
         if (x > max_val) {
@@ -19,10 +19,10 @@ namespace
 
 /* BMP结构说明
 typedef  struct  tagBITMAPFILEHEADER {
-    unsigned short int  bfType;       //位图文件的类型，必须为BM 
+    unsigned short int  bfType;       //位图文件的类型，必须为BM
     unsigned long       bfSize;       //文件大小，以字节为单位
-    unsigned short int  bfReserverd1; //位图文件保留字，必须为0 
-    unsigned short int  bfReserverd2; //位图文件保留字，必须为0 
+    unsigned short int  bfReserverd1; //位图文件保留字，必须为0
+    unsigned short int  bfReserverd2; //位图文件保留字，必须为0
     unsigned long       bfbfOffBits;  //位图文件头到数据的偏移量，以字节为单位
 }BITMAPFILEHEADER;
 
@@ -31,7 +31,7 @@ typedef  struct  tagBITMAPINFOHEADER
     long       biSize;               //该结构大小，字节为单位
     long       biWidth;              //图形宽度以象素为单位
     long       biHeight;             //图形高度以象素为单位
-    short int  biPlanes;             //目标设备的级别，必须为1 
+    short int  biPlanes;             //目标设备的级别，必须为1
     short int  biBitcount;           //颜色深度，每个象素所需要的位数
     short int  biCompression;        //位图的压缩类型
     long       biSizeImage;          //位图的大小，以字节为单位
@@ -118,10 +118,10 @@ int Convert::Rgb24ToYUV420(unsigned char* rgbBuf, int width, int height, unsigne
     ptrU = yuvBuf + width * height;
     ptrV = ptrU + (width * height * 1 / 4);
     unsigned char y, u, v, r, g, b;
-    for (int j = 0; j < height; j++) 
+    for (int j = 0; j < height; j++)
     {
         ptrRGB = rgbBuf + width * j * 3;
-        for (int i = 0; i < width; i++) 
+        for (int i = 0; i < width; i++)
         {
             r = *(ptrRGB++);
             g = *(ptrRGB++);
@@ -130,12 +130,12 @@ int Convert::Rgb24ToYUV420(unsigned char* rgbBuf, int width, int height, unsigne
             u = (unsigned char)((-38 * r - 74 * g + 112 * b + 128) >> 8) + 128;
             v = (unsigned char)((112 * r - 94 * g - 18 * b + 128) >> 8) + 128;
             *(ptrY++) = ClipValue(y, 0, 255);
-            if (j % 2 == 0 && i % 2 == 0) 
+            if (j % 2 == 0 && i % 2 == 0)
             {
                 *(ptrU++) = ClipValue(u, 0, 255);
             }
             else {
-                if (i % 2 == 0) 
+                if (i % 2 == 0)
                 {
                     *(ptrV++) = ClipValue(v, 0, 255);
                 }
@@ -145,30 +145,26 @@ int Convert::Rgb24ToYUV420(unsigned char* rgbBuf, int width, int height, unsigne
     return true;
 }
 
+// YUV420p：每4个 Y分量 共用一组 UV分量，前面全是Y分量，后面是U分量，再后面是V分量
 int Convert::Rgb32ToYUV420(unsigned char* rgbBuf, int width, int height, unsigned char** yuvBuf)
 {
-    int yuv420Size = width * height * 3 / 2;
+    int yuv420Size = width * height * 3 / 2; // Ysize:(width*height); Usize:(width*height)/2; Vsize:(width*height)/2;
     *yuvBuf = new unsigned char[yuv420Size];
 
-    //source-stride
-    int Dst_Stride_Y = width;
-    const int uv_stride = (width + 1) / 2;
+    const int yStride = width;
+    const int uvStride = (width + 1) / 2;
 
-    //source-length
-    const int y_length = width * height;
-    int uv_length = uv_stride * ((height + 1) / 2);
+    const int yLength = width * height;
+    const int uvLength = uvStride * ((height + 1) / 2);
 
-    //source-data
-    unsigned char* Y_data_Dst = *yuvBuf;
-    unsigned char* U_data_Dst = *yuvBuf + y_length;
-    unsigned char* V_data_Dst = U_data_Dst + uv_length;
+    unsigned char* yDataDstPtr = *yuvBuf;
+    unsigned char* uDataDstPtr = *yuvBuf + yLength;
+    unsigned char* vDataDstPtr = uDataDstPtr + uvLength;
 
-    //BGRAToI420, 内存顺序是BGRA,所以用方法得反过来ARGB
-    libyuv::ARGBToI420(rgbBuf,
-        width * 4,
-        Y_data_Dst, Dst_Stride_Y,
-        U_data_Dst, uv_stride,
-        V_data_Dst, uv_stride,
+    libyuv::ARGBToI420(rgbBuf, width * 4,// 带转换的argb数据；argb数据每一行的大小
+        yDataDstPtr, yStride,            // 用于保存y分量的指针；每一行y分量的长度
+        uDataDstPtr, uvStride,           // 用于保存u分量的指针；每一行u分量的长度
+        vDataDstPtr, uvStride,           // 用于保存v分量的指针；每一行v分量的长度
         width, height);
 
     return yuv420Size;
