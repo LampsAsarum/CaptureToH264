80707d97eea38d295ac6b4e04d2f0711b1527489
xingzixin <xingzixin@cvte.com>
2022-09-29 10:57:33 +0800

    [feature]渲染添加注释

--- CaptureToH264/CaptureToH264/Direct3DRender.cpp
+++ CaptureToH264/CaptureToH264/Direct3DRender.cpp
@@ -21,6 +21,7 @@ bool Direct3DRender::Init(HWND hwnd, unsigned int width, unsigned int height)
 	InitializeCriticalSection(&m_critial);
 	Cleanup();
 
+	// 创建一个IDirect3D9接口
 	m_pDirect3D9 = Direct3DCreate9(D3D_SDK_VERSION);
 	if (m_pDirect3D9 == NULL) {
 		return false;
@@ -28,26 +29,34 @@ bool Direct3DRender::Init(HWND hwnd, unsigned int width, unsigned int height)
 
 	D3DPRESENT_PARAMETERS d3dpp;
 	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	// 设为true则为窗口模式，false则为全屏模式
 	d3dpp.Windowed = TRUE;
+	// 指定表面在交换链中是如何被交换的。
+	// D3DSWAPEFFECT_DISCARD:后台缓冲表面区的东西被复制到屏幕上后,后台缓冲表面区的东西就没有什么用了,可以丢弃了。
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	// 后台缓冲表面的像素格式
 	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
 
 	GetClientRect(hwnd, &m_rtViewport);
 
+	// 创建一个Device
+	// D3DADAPTER_DEFAULT 指明使用的物理设备是主要的显示器适配器。
+	// D3DDEVTYPE_HAL 硬件加速
 	lRet = m_pDirect3D9->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hwnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &m_pDirect3DDevice);
+	if (FAILED(lRet)) {
+		return false;
+	}
+
+	// 创建一个Surface离屏表面
+	lRet = m_pDirect3DDevice->CreateOffscreenPlainSurface(width, height, (D3DFORMAT)MAKEFOURCC('Y', 'V', '1', '2'), D3DPOOL_DEFAULT, &m_pDirect3DSurfaceRender, NULL);
 	if (FAILED(lRet)) {
 		return false;
 	}
 
 	m_width = width;
 	m_height = height;
-	lRet = m_pDirect3DDevice->CreateOffscreenPlainSurface(width, height, (D3DFORMAT)MAKEFOURCC('Y', 'V', '1', '2'), D3DPOOL_DEFAULT, &m_pDirect3DSurfaceRender, NULL);
 
-	if (FAILED(lRet)) {
-		return false;
-	}
-
-	return 0;
+	return true;
 }
 
 void Direct3DRender::Cleanup()
@@ -73,7 +82,12 @@ bool Direct3DRender::Render(unsigned char* buffer)
 
 	HRESULT lRet;
 	D3DLOCKED_RECT d3d_rect;
-	lRet = m_pDirect3DSurfaceRender->LockRect(&d3d_rect, NULL, D3DLOCK_DONOTWAIT);
+	// LockRect()锁定纹理上的一块矩形区域，该矩形区域被映射成像素数组。
+	// 参数：
+	// pLockedRect: 返回的一个D3DLOCKED_RECT结构体用于描述被锁定的区域。
+	// pRect : 使用一个 RECT结构体指定需要锁定的区域。如果为NULL的话就是整个区域。
+	// Flags : https://learn.microsoft.com/en-us/windows/win32/direct3d9/d3dlock。 使用 D3DLOCK_DONOTWAIT 可能会造成丢失YUV图像
+	lRet = m_pDirect3DSurfaceRender->LockRect(&d3d_rect, NULL, D3DLOCK_DISCARD);
 	if (FAILED(lRet)) {
 		return false;
 	}
